df061d46c122fe4545c5ff853c922a951254d185
juliettelinkenheld <linkenheld@hotmail.com>
2024-04-19 16:47:06 +0200

    politic conf

--- lib/menu.php
+++ lib/menu.php
@@ -1,10 +1,10 @@
 <?php
 
 $mainMenu = [
-    "index.php"=> [ "menu_title" => "Accueil", "head_title" => "Accueil Musique DJPAK", "meta_description" => "DJPAK, rentrée dans l'univers musical, envoyer un message, écouter"],
+    "index.php"=> [ "menu_title" => "Accueil", "head_title" => "Accueil Musique DJPAK", "meta_description" => "DJPAK, rentrée dans l'univers musical, envoyer un message, écouter, télécharger et partager"],
     "apropos.php"=> ["menu_title" => "À propos", "head_title" => "A propos","meta_description" => "découvrez qui est DJPAK"],
     "music.php"=> ["menu_title" => "Musique DJPAK", "head_title" => "Musique de DJPAK","meta_description" => "Rentrée dans l'univers musical de DJPAK"],
-    "form.php"=> ["menu_title" => "Contact", "head_title" => "contact","meta_description" => "contactez-nous"],
+    "form.php"=> ["menu_title" => "Contact", "head_title" => "contact","meta_description" => "contactez-nous DJPAK vous répondra"],
     "opinions.php"=> ["menu_title" => "Avis", "head_title" => "avis","meta_description" => "donnez votre avis sur la musique de DJPAK"],
     "newsLetter.php"=> ["menu_title" => "Newsletter", "head_title" => "Newsletter","meta_description" => "inscrivez-vous à la newsletter"],
     "login.php"=> ["menu_title" => "login", "head_title" => "connexion","meta_description" => "Connecter à l'espace professionnel", "exclude"=> true],
--- templates/footer_fond.php
+++ templates/footer_fond.php
@@ -31,26 +31,46 @@
 </div>
 <!-- Contenu de la politique de confidentialité -->
 <div id="privacy-policy-popup">
-    <h2>Votre politique de confidentialité </h2>
+    <h2>Politique de confidentialité de DJPAK </h2>
     <div class="overflow-auto" style="max-height: 300px;">
-        <p> Dernière mise à jour : 10/2023
-            
-            Lorsque vous utilisez notre formulaire de contact pour demander des informations sur une voiture d'occasion spécifique, nous collectons les informations que vous fournissez volontairement, telles que votre nom, votre adresse e-mail et votre numéro de téléphone.
-            Finalité de la collecte :
-            Les informations collectées sont utilisées exclusivement pour répondre à votre demande d'informations sur la voiture d'occasion spécifique que vous avez mentionnée dans le formulaire.
-            Utilisation des informations :
-            Nous utilisons les informations que vous fournissez pour vous fournir les détails demandés sur la voiture d'occasion spécifique et pour faciliter la communication relative à votre demande.
-            Conservation des données :
-            Les informations fournies dans le cadre de votre demande sont conservées pendant six mois à des fins de suivi et de communication ultérieure.
-            Communication additionnelle :
-            En fournissant vos informations, vous consentez à recevoir des communications additionnelles de notre part, notamment des mises à jour sur de nouvelles arrivées de voitures d'occasion et des offres promotionnelles.
-            Sécurité des informations :
-            Nous prenons des mesures de sécurité appropriées pour protéger vos informations, y compris des technologies de sécurité et des pratiques de gestion sécurisées.
-            Partage d'informations :
-            Nous ne partageons pas vos informations avec des tiers sans votre consentement, sauf si cela est nécessaire pour répondre à votre demande spécifique (par exemple, pour fournir un rapport d'historique de véhicule).
-            En utilisant notre site et en fournissant vos informations, vous consentez à notre politique de confidentialité. Nous nous réservons le droit de mettre à jour cette politique de confidentialité à tout moment. Veuillez consulter cette page régulièrement pour être informé des éventuelles modifications.
-            Si vous avez des questions ou des préoccupations concernant notre politique de confidentialité, veuillez nous contacter.
-        </p>
+        <p> La confidentialité des visiteurs de DJPAK est très importante pour nous. Cette politique de confidentialité décrit les types d'informations personnelles collectées et enregistrées par DJPAK et la manière dont nous les utilisons.
+
+Si vous avez des questions supplémentaires ou avez besoin de plus d'informations sur notre politique de confidentialité, n'hésitez pas à nous contacter via l'onglet de contact sur notre site web.
+
+Consentement
+
+En utilisant notre site web, vous consentez à notre politique de confidentialité et acceptez ses conditions.
+
+Informations que nous collectons
+
+Lorsque vous visitez DJPAK, nous collectons et enregistrons automatiquement certaines informations sur votre appareil, votre navigateur et votre comportement sur le site. Ces informations peuvent inclure votre adresse IP, le type de navigateur, les pages que vous avez visitées et d'autres statistiques.
+
+De plus, si vous choisissez de nous fournir des informations personnelles via notre formulaire de contact, notre formulaire d'avis ou notre formulaire d'inscription à la newsletter, nous collecterons les informations que vous fournissez volontairement, telles que votre nom, votre adresse e-mail et d'autres détails que vous choisissez de nous communiquer.
+
+Utilisation des informations
+
+Les informations que nous collectons sur DJPAK sont utilisées pour améliorer l'expérience des utilisateurs en personnalisant notre site web et en fournissant du contenu et des publicités pertinents. Nous utilisons également ces informations pour communiquer avec vous, répondre à vos demandes et vous envoyer des newsletters si vous avez choisi de vous y abonner.
+
+Nous ne partageons pas vos informations personnelles avec des tiers, sauf si cela est nécessaire pour répondre à une demande que vous avez soumise ou si cela est exigé par la loi.
+
+Conservation des données
+
+Nous conservons les informations collectées aussi longtemps que nécessaire pour fournir les services demandés par les utilisateurs et pour respecter nos obligations légales.
+
+Vos droits
+
+Vous avez le droit de demander l'accès, la correction ou la suppression de vos informations personnelles que nous détenons. Vous avez également le droit de retirer votre consentement à tout moment.
+
+Mises à jour de la politique de confidentialité
+
+Nous nous réservons le droit de mettre à jour ou de modifier notre politique de confidentialité à tout moment. Les modifications apportées à cette politique seront affichées sur cette page et entreront en vigueur dès leur publication.
+
+En continuant à utiliser DJPAK après la publication de ces modifications, vous acceptez ces modifications et reconnaissez avoir pris connaissance de la politique de confidentialité mise à jour.
+
+Contactez-nous
+
+Si vous avez des questions ou des préoccupations concernant notre politique de confidentialité ou notre utilisation de vos informations personnelles, veuillez nous contacter via l'onglet de contact sur notre site web.
+</p>
     </div>
     <button id="close-popup">Fermer</button>
 </div>
